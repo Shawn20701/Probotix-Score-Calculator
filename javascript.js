d54df8c003d4297d5658244702f44b7dd1d7ef0b
Shawn20701 <147204664+Shawn20701@users.noreply.github.com>
2025-06-26 12:50:31 -0400

    added the offical scoring catagories

--- javascript.js
+++ javascript.js
@@ -14,14 +14,24 @@ function updateRecent() {
 function SubmitForm(){
 const form = document.getElementById('Main_Form');
 const Door_Value = Number(form.elements['Num_of_Door'].value);
-var Door_Points = Door_Value * 2;
+var Door_Points = Door_Value * 1;
 const Obstacle_Value = Number(form.elements['Times_Course_Completed'].value);
-var Obstacle_Points = Obstacle_Value * 6;
+var Obstacle_Points = Obstacle_Value * 10;
+const Obstacle_Penalties = Number(form.elements['Times_Obstacles_Hit'].value);
+var Obstacle_Penalties_points = Obstacle_Penalties * 2
+const Block_Value = Number(form.elements['Blocks_Pushed'].value);
+var Block_Points = Block_Value * 2;
+const Block_Bonus = Number(form.elements['Bonus'].value);
+var Block_Bonus_Points = Block_Bonus * 2;
 const Pixel_Value = Number(form.elements['Pixels_Sorted'].value);
-var Pixel_Points = Pixel_Value * 4;
+var Pixel_Points = Pixel_Value * 5;
 const Hill_Value = Number(form.elements['Ducks_Push_Up_Hill'].value);
-var Hill_Points = Hill_Value * 7;
-var Total = Door_Points + Obstacle_Points + Pixel_Points + Hill_Points;
+var Hill_Points = Hill_Value * 10;
+const Major_Value = Number(form.elements['Major_Penalties'].value);
+const Minor_Value = Number(form.elements['Minor_Penalties'].value);
+var Major_Points = Major_Value * 5;
+var Minor_Points = Minor_Value * 3;
+var Total = Door_Points + Obstacle_Points - Obstacle_Penalties_points + Block_Points + Block_Bonus_Points + Pixel_Points + Hill_Points - Major_Points - Minor_Points;
 console.log(Total);
 document.getElementById("Form_Output").innerHTML = Total;
 const team = document.querySelector('input[name="Team"]:checked')?.value;
